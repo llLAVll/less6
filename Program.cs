83fb50671e72e84ed1add87a5e8b2aee18396afd
Алексей <vipaleksei007@gmail.com>
2024-06-10 15:37:46 +0300

    Program process out and process kill

--- Program.cs
+++ Program.cs
@@ -1,24 +1,34 @@
-﻿Console.WriteLine("[sys_mes]: Добро пожаловать в Task Manager: ");
+﻿using less6;
+
+Console.WriteLine("[sys_mes]: Добро пожаловать в Task Manager: ");
 int numTask = -1;
 do
 {
     Console.WriteLine(
     "\n Выберете действие:" +
     "\n 1 - Вывод процессов" +
-    "\n 2 - Завершить процесс + " +
+    "\n 2 - Завершить процесс по ID " +
+    "\n 3 - Завершить процесс по Имени " +
     "\n 0 - Выход");
 
     numTask = Convert.ToInt32(Console.ReadLine());
     switch (numTask)
     {
         case 0:
-
+            Console.WriteLine("[sys_mes]: До скорой встречи.");
             break;
         case 1:
-
+            new ProcessComands().OutProses();
             break;
         case 2:
-
+            Console.Write("[sys_mes]: Введите id процесса: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            new ProcessComands().ExitProsesId(id);
+            break;
+        case 3:
+            Console.Write("[sys_mes]: Введите название процесса:");
+            string name = Console.ReadLine();
+            new ProcessComands().ExitProsesName(name);
             break;
         default:
             Console.WriteLine("[sys_mes]: Такой команды нет.");
--- ProsesComands.cs
+++ ProsesComands.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 namespace less6
 {
-    internal class ProsesComands
+    class ProcessComands
     {
+        public void OutProses()
+        {
+            Console.WriteLine("[sys_mes]:Вывод процессов: ");
+            Process[] proc = Process.GetProcesses();
+            foreach (Process p in proc)
+            {
+                Console.WriteLine("Name: " + p.ProcessName + "\t \t \tId: " + p.Id);
+            }
+
+        }
+
+        public void ExitProsesName(string name)
+        {
+            try
+            {
+                Process[] proc = Process.GetProcessesByName(name);
+                foreach (Process p in proc)
+                    p.Kill();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[sys_mes]: Ошибка закрытия процесса по name: \n[sys_mes]: " + ex.Message);
+                throw;
+            }
+            finally { Console.WriteLine("[sys_mes]: Процесс успешно остановлен."); }
+
+        }
+
+        public void ExitProsesId(int id)
+        {
+            try
+            {
+                Process proc = Process.GetProcessById(id);
+                if (proc.Id == id) proc.Kill();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[sys_mes]: Ошибка закрытия процесса по id: \n[sys_mes]: " + ex.Message);
+                throw;
+            }
+            finally { Console.WriteLine("[sys_mes]: Процесс успешно остановлен."); }
+        }
+
     }
 }

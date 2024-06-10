56c4686aa7bf0ebddf5d9fb3dceb52c8bfa8b1e8
Алексей <vipaleksei007@gmail.com>
2024-06-10 14:55:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("[sys_mes]: Добро пожаловать в Task Manager: ");
+int numTask = -1;
+do
+{
+    Console.WriteLine(
+    "\n Выберете действие:" +
+    "\n 1 - Вывод процессов" +
+    "\n 2 - Завершить процесс + " +
+    "\n 0 - Выход");
+
+    numTask = Convert.ToInt32(Console.ReadLine());
+    switch (numTask)
+    {
+        case 0:
+
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        default:
+            Console.WriteLine("[sys_mes]: Такой команды нет.");
+            break;
+    }
+} while (numTask != 0);
+
